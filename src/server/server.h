ef817ef3fa63582ad6bf083b75b3d18361de0d05
Alberto Damo <alberto.damo@gmail.com>
2024-04-01 11:08:55 +0200

    introduced standard type system

--- src/server/server.h
+++ src/server/server.h
@@ -1,18 +1,19 @@
 #pragma once
 
 #include "../common/net_node.h"
+#include <stdint.h>
 
 typedef struct server server;
 
-int server_init(server** sv, const address* addr, const unsigned int sv_capacity);
-int server_start(server* sv);
-int server_stop(const server* sv);
-int server_kill(const server* sv);
+uint8_t server_init(server** sv, const address* addr, const uint16_t sv_capacity);
+uint8_t server_start(server* sv);
+uint8_t server_stop(const server* sv);
+uint8_t server_kill(const server* sv);
 void server_wait(const server* sv);
-int server_free(server* sv);
-int server_send(const server* sv, const address* addr);
-int server_recv(const server* sv, unsigned char* buffer, unsigned int* buffer_size);
-unsigned long server_sizeof();
-const char* server_addr_str(const server* sv);
+uint8_t server_free(server* sv);
+uint8_t server_send(const server* sv, const address* addr, const void* data, const uint16_t data_amount);
+uint8_t server_recv(const server* sv, void* buffer, uint16_t* buffer_size);
+uint64_t server_sizeof();
+const uint8_t* server_addr_str(const server* sv);
 void server_to_string(const server* sv);
 const c_vector* server_get_client_list(const server* sv);
